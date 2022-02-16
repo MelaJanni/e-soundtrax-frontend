b9379f81f7352112aea704b5be4bb241df69513a
MelaJanni <98432127+MelaJanni@users.noreply.github.com>
2022-02-16 16:57:50 -0300

    V1

---
+++ assets/js/carousel_vue.js
@@ -0,0 +1,10 @@
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+var app = new Vue({
+    el: '#app',
+    data: {
+        slides: 7
+    },
+    components: {
+        Carousel3d, Slide
+    }
+  })
